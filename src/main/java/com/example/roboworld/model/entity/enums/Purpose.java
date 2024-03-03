a3859d5771401138b98dd97bca2b49be2376c8a8
vanesailieva06 <vanesa_h_ilieva@abv.bg>
2024-03-03 21:24:03 +0200

    Design changes, delete Add robot and instead it is added courses

--- src/main/java/com/example/roboworld/model/entity/enums/Purpose.java
+++
@@ -1,5 +0,0 @@
-package com.example.roboworld.model.entity.enums;
-
-public enum Purpose {
-    EDUCATIONAL,ENTERTAINMENT, INFORMATIONAL, HELPFUL
-}
--- src/main/java/com/example/roboworld/repository/RobotRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.roboworld.repository;
-
-import com.example.roboworld.model.entity.Robot;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RobotRepository extends JpaRepository<Robot, Long> {
-}
